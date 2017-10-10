d7a59a78d08c6c73d28370d1c827a04b5b541817
Nicolò Chieffo <n.chieffo@tecla.it>
2017-10-10 14:00:40 +0200

    rimossa estrazione request dal LoggingFilter

--- src/it/tecla/utils/logging/LoggingFilter.java
+++ src/it/tecla/utils/logging/LoggingFilter.java
@@ -1,24 +1,17 @@
 package it.tecla.utils.logging;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -30,22 +23,12 @@ public class LoggingFilter implements Filter {
 	public static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
 	public static final Marker SKIP_STDOUT_MARKER = MarkerFactory.getMarker("SKIP_STDOUT");
 	
-	private String urlType;
-	private boolean extractBody;
-
 	public void init(FilterConfig config) throws ServletException {
 		
-		urlType = config.getInitParameter("url_type");
-		if (urlType == null) {
-			urlType = "path_info";
-		}
-		
-		extractBody = StringUtils.equals(config.getInitParameter("extract_body"), "true");
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		
-		String requestBody = null;
 		String remoteUser = null;
 		HttpSession session = null;
 		String contentType = null;
@@ -58,17 +41,11 @@ public class LoggingFilter implements Filter {
 				final HttpServletRequest httpRequest = (HttpServletRequest) request;
 				
 				StringBuffer url;
-				if (urlType.equals("full")) {
-					url = httpRequest.getRequestURL();
-				} else if (urlType.equals("context_path")) {
-					url = new StringBuffer(httpRequest.getContextPath() + httpRequest.getServletPath() + httpRequest.getPathInfo());
-				} else if (urlType.equals("servlet_path")) {
-					url = new StringBuffer(httpRequest.getServletPath() + httpRequest.getPathInfo());
-				} else if (urlType.equals("path_info")) {
-					url = new StringBuffer(httpRequest.getPathInfo());
-				} else {
-					url = new StringBuffer(httpRequest.getServletPath() + httpRequest.getPathInfo());
-				}
+				
+//				url = httpRequest.getRequestURL();
+//				url = new StringBuffer(httpRequest.getContextPath() + httpRequest.getServletPath() + httpRequest.getPathInfo());
+				url = new StringBuffer(httpRequest.getServletPath() + httpRequest.getPathInfo());
+//				url = new StringBuffer(httpRequest.getPathInfo());
 				
 				if (httpRequest.getQueryString() != null) {
 					url.append("?");
@@ -89,43 +66,6 @@ public class LoggingFilter implements Filter {
 	//			if (contentType != null) {
 	//				MDC.put("req.contentType", contentType);
 	//			}
-				
-				if (extractBody && StringUtils.containsAny(contentType, "application/json", "text/json", "application/x-www-form-urlencoded", "text/plain")) {
-					
-					Reader reader = httpRequest.getReader();
-					final String requestBodyTemp = IOUtils.toString(reader);
-					requestBody = requestBodyTemp;
-					
-	//				if (reader != null) {
-	//					if (StringUtils.isNotBlank(requestBody)) {
-	//						MDC.put("req.body", requestBody);
-	//					}
-	//				}
-					
-					request = new HttpServletRequestWrapper(httpRequest) {
-						
-						BufferedReader reader = new BufferedReader(new StringReader(requestBodyTemp));
-						
-						@Override
-						public ServletInputStream getInputStream() throws IOException {
-							
-							return new ServletInputStream() {
-								
-								@Override
-								public int read() throws IOException {
-									return reader.read();
-								}
-								
-							};
-						}
-						
-						@Override
-						public BufferedReader getReader() throws IOException {
-							return reader;
-						}
-						
-					};
-				}
 	
 				if (contentType != null) {
 					req = httpRequest.getMethod() + "(" + contentType + ") " + url;
@@ -144,6 +84,8 @@ public class LoggingFilter implements Filter {
 			chain.doFilter(request, response);
 			
 		} catch(Throwable t) {
+			
+			String requestBody = MDC.get("req.body");
 			
 			StringBuilder sb = new StringBuilder();
 			sb.append("Uncaught exception while serving request");
